e519a029930c1098a2211e73960367aaf6bf9b58
Michael Severino <mikeyseverino@gmail.com>
2022-01-15 15:16:25 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feetNumber){
+  if (feetNumber > 2500)
+    return 'No can do.'
+  else if (feetNumber >= 2000) 
+    return 'I will gladly take your thirty bucks.'
+  else if (feetNumber <= 400)
+    return 'This one is on me!'
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(someCity){
+return (someCity === 'NYC' ? 'Ok, sounds good.' : 'No go.');
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tipCharm){
+switch (tipCharm){
+  case 'generous':
+    return 'Thank you so much.';
+    break;
+  case 'not as generous':
+    return 'Thank you.'
+    break;
+  default:
+    return 'Bye.';
+    break;
+}
 }
